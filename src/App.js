c5e67371b2243660e552ac5f368f467c5c025179
ibktommy <atomquartz@gmail.com>
2022-10-01 07:07:41 +0100

    Remove some functionality from the App-Component as its not needed there since get whatever states or functions from the AppContext in other child-components without using prop-drilling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React, { useState } from 'react'
 import Inputs from './components/Inputs';
+import { useGlobalAppContext } from './context/context';
 import TaskList from './TaskList';
 
 function App() {
-  // APP STATE
-  const [data, setData] = useState([])
-
-
-  // Function To Get Data Prop From Input-Child-Component
-  const getTaskData = (newTaskData) => {
-    setData([...data, newTaskData])
-  }
-
-  // Function To Delete an Item from the Data-Array
-  const deleteTaskItem = (id) => {
-    const filteredTasks = data.filter((task) => task.id !== id)
-    setData(filteredTasks)
-  }
+  // Get Data-State from the App Context
+  const { data } = useGlobalAppContext()
 
   return (
     <div className='container'>
@@ -25,10 +14,10 @@ function App() {
         <span>Your Personal Tasklist Web App</span>
       </h2>
 
-      <Inputs onGetTaskData={getTaskData} />
+      <Inputs />
 
       {/* Display TaskList Component only when we have tasks submitted */}
-      {data.length > 0 && <TaskList data={data} passDelete={deleteTaskItem}/>}
+      {data.length > 0 && <TaskList />}
     </div>
   );
 }
--- src/context/context.js
+++ src/context/context.js
@@ -1,10 +1,31 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
+  // Setting Data-App-State
+  const [data, setData] = useState([])
+
+
+  // Function To Get Data Prop From Input-Child-Component
+  const getTaskData = (newTaskData) => {
+    setData([...data, newTaskData])
+  }
+
+  // Function To Delete an Item from the Data-Array
+  const deleteTaskItem = (id) => {
+    const filteredTasks = data.filter((task) => task.id !== id)
+    setData(filteredTasks)
+  }
+
+
   return (
-    <AppContext.Provider value={'Hello'}>
+    <AppContext.Provider value={{
+      data,
+      setData,
+      getTaskData,
+      deleteTaskItem,
+    }}>
       {children}
     </AppContext.Provider>
   )
