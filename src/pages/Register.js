b506d583161716e6160c1d8f0f8db73407f9e290
ibktommy <atomquartz@gmail.com>
2022-10-02 22:21:49 +0100

    Create Register Page with styles

---
+++ src/pages/Register.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+const Register = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault()
+  }
+  return (
+    <main className='form-main'>
+      <h3 className='form-title'>Register Your Account</h3>
+
+      <form onSubmit={formSubmitHandler}>
+        <div className="username">
+          <input type="text" placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
+        </div>
+        <div className="password">
+          <input type="text" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+
+        <button className="submit-form">
+          Click to Register
+        </button>
+
+        <p>
+          Already have an Account?
+          <span>REGISTER</span>
+        </p>
+      </form>
+    </main>
+  )
+}
+
+export default Register
