f826d0f20615cd79c4de4fd1ca7db303f57036b3
ibktommy <atomquartz@gmail.com>
2022-10-03 18:08:15 +0100

    Get username from firebase-user-object value and display it in the username component

--- src/components/UserName.js
+++ src/components/UserName.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useFirebaseContext } from '../context/firebaseContext'
 
 const UserName = () => {
+  const { user } = useFirebaseContext()
+
+  let username = user.email.slice(0, 3);
+
   return (
     <h3 className="name">
-      Hello Best!
+      Hello {username}
     </h3>
   )
 }
